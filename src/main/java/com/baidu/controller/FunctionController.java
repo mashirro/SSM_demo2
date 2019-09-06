835fc66cfab5ec41bb52ca40957964206b70af8a
chenzg <1213410472@qq.com>
2019-09-06 17:19:42 +0800

    删除多余引用

--- src/main/java/com/baidu/controller/FunctionController.java
+++ src/main/java/com/baidu/controller/FunctionController.java
@@ -1,18 +1,14 @@
 package com.baidu.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.baidu.entity.Function;
 import com.baidu.service.FunctionService;
-
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 
 @Controller
 @RequestMapping("/function")
@@ -56,4 +52,6 @@ public class FunctionController {
 		}
 		return children;
 	}
+	
+	
 }
